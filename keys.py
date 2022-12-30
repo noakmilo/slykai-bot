77c4ca7dd6847a1c5d2f243755555f82f1956ea6
Camilo Noa <camilo@slyk.io>
2022-12-29 21:13:29 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit https://dev.twitter.com to create an application and get your keys
+keys = dict(
+	consumer_key =          'XXXXXXXXXXXXXXXXXXXXXXX',
+	consumer_secret =       'XXXXXXXXXXXXXXXXXXXXXXX',
+	access_token =          'XXXXX-XXXXXXXXXXXXXXXXXXXXXXX',
+	access_token_secret =   'XXXXXXXXXXXXXXXXXXXXXXX',
+)
