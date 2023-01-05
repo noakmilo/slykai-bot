a90acdd771829886cb7ab5bb139a9402e9ea5c9b
Camilo Noa <camilo@slyk.io>
2023-01-05 11:52:57 -0500

    Adding the reply_with_chatgpt_prompt function

--- bot.py
+++ bot.py
@@ -31,11 +31,9 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
-def search_and_reply(query):
-    """
-    Get the latest replies with the string criteria
-    """
-    for tweet in tweepy.Cursor(api.search_tweets, q=query, lang='en').items(1):
+def search_and_reply(string):
+       # Get the latest replies with the string criteria
+    for tweet in tweepy.Cursor(api.search_tweets, q=string, lang='en').items(1):
         replies = api.search_tweets(q=f"to:{tweet.user.screen_name}", since_id=tweet.id)
         if replies:    
             continue
@@ -47,7 +45,6 @@ def search_and_reply(query):
             )
         except StopIteration:
             break
-
 
 def search_and_reply2(string2):
     # Get the latest replies with the string2 criteria
@@ -65,42 +62,60 @@ def search_and_reply2(string2):
         except StopIteration:
             break
 
+def generate_idea(tweet_text):
+    tweet_text = tweet_text.replace("@slyk_ai give me ", "")
+    promp = "In 140 characters: " + tweet_text + "\n"
 
-def reply_with_chatgpt_prompt(user_request):
-    model_engine = "text-davinci-002"
-    completion = openai.Completion.create(engine=model_engine, prompt=user_request, max_tokens=120, n=1,stop=None,temperature=1)
-    response = completion.choices[0].text
-   
-    # Get the latest replies with the user_request criteria
 
-    for tweet in tweepy.Cursor(api.search_tweets, q=user_request, lang='en').items(1):
-        replies = api.search_tweets(q=f"to:{tweet.user.screen_name}", since_id=tweet.id)
-        if replies:  
-              
-            continue
+    # uses the OpenAI API to generate an idea based on the tweet.
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=promp,
+        temperature=1,
+        max_tokens=90,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=1
+    )
+    idea = response.choices[0].text
+    print(idea)
+    return idea
+    
         
-        try:
+
+def reply_with_chatgpt_prompt():
+    with open("replied_tweets.json", "r") as f:
+        replied_tweets = json.load(f)
+
+    for tweet in tweepy.Cursor(api.mentions_timeline).items():
+        username = tweet.user.screen_name
+        tweet_id = tweet.id
+        tweet_text = tweet.text
+       
+        if tweet.in_reply_to_status_id is not None or tweet_id in replied_tweets:
+            continue
+        if "@slyk_ai give me" in tweet_text.lower():
+            idea = generate_idea(tweet_text)
             api.update_status(
-                f"@{tweet.user.screen_name} Here is your #StartupIdea generated with #ChatGPT👇 \n\n {response} \n\nJoin the https://ai.slyk.io community to earn $AI and launch this idea with Slyk.",
-                in_reply_to_status_id=tweet.id
-            )     
-        except StopIteration:
-            break
-                        
+                f"@{username} {idea} \n\nJoin the https://ai.slyk.io community to earn $AI and redeem it to start this idea.",
+                in_reply_to_status_id=tweet_id
+            )
+            replied_tweets.append(tweet_id)
 
-
-if __name__ == "__main__":
-    while True:
-        try:
-            search_and_reply("@slyk_ai launch a startup")
-            search_and_reply2("@slyk_ai make a slyk clone")
-            reply_with_chatgpt_prompt("@slyk_ai give me a startup idea")
-            time.sleep(800)
-        except tweepy.errors.Forbidden as e:
-            if e.api_codes == 187:
-                # duplicate tweet error
-                print("Error: Duplicate tweet")
-            else:
-                # other error
-                print(e)
-
+    with open("replied_tweets.json", "w") as f:
+        json.dump(replied_tweets, f)
+        
+while True:
+    try:
+        search_and_reply("@slyk_ai launch a startup")
+        search_and_reply2("@slyk_ai make a slyk clone")
+        reply_with_chatgpt_prompt()
+        time.sleep(60)
+    except tweepy.errors.Forbidden as e:
+        if e.api_codes == 187:
+            # duplicate tweet error
+            print("Error: Duplicate tweet")
+        else:
+            # other error
+            print(e)
+            
